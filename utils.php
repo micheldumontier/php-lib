ddd11e51c68b270ed7b99d6245bb88c7f7e0a9e5
Michel Dumontier <michel.dumontier@gmail.com>
2012-07-30 22:48:35 -0400

    added new function

--- utils.php
+++ utils.php
@@ -79,34 +79,39 @@ class Utils
 	
 	public static function FTPDownload($host,$files,$ldir, $user='anonymous',$pass='myemail@email.com',$passive_mode = true)
 	{
-		echo "connecting to $host ...";
 		$ftp = ftp_connect($host);
 		if(!$ftp) {
 			trigger_error("Unable to connect to $host");
 			return FALSE;
 		}
-		ftp_pasv ($ftp, $passive_mode) ;				
 		$login = ftp_login($ftp, $user, $pass);
-		if ((!ftp) || (!$login)) { 
+		if ((!$ftp) || (!$login)) { 
 			trigger_error("Unable to login to $host with user:$user and pass:$pass");
 			return FALSE;
 		} else {
-			echo "Connected ...";
+			echo "Connected to $host ...";
 		}
+		ftp_pasv ($ftp, $passive_mode) ;				
 				
 		// download
 		foreach($files AS $filepath) {
-			if(($pos = strrpos($filepath,'/')) !== FALSE) {
+			if(($pos = strrpos($filepath,'/')) === FALSE) {
 				$rdir = '';
 				$file = $filepath;
 			} else {
 				$rdir = substr($filepath,0,$pos);
+				ftp_chdir($ftp,$rdir);
 				$file = substr($filepath,$pos+1);
 			}
+
 			echo "Downloading $file ...";
-			if(ftp_get($ftp, $ldir.$file, $file, FTP_BINARY) === FALSE) {
-				trigger_error("Error in downloading $file");
-				continue;
+			$ret = ftp_nb_get($ftp, $ldir.$file, $file , FTP_BINARY);
+//			while(($ret=ftp_nb_continue($ftp))==FTP_MOREDATA){}
+//			while(($ret=ftp_nb_continue($ftp))==FTP_MOREDATA){}
+//			while(($ret=ftp_nb_continue($ftp))==FTP_MOREDATA){}
+			if ($ret != FTP_FINISHED) {
+			   echo "Error in downloading $file...";
+				return FALSE;
 			}
 		}
 		if(isset($ftp)) ftp_close($ftp);
