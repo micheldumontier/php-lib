75d645542c480b5877adb3ef4219d3c7ecbbf355
Michel Dumontier <michel.dumontier@gmail.com>
2012-09-03 14:01:57 +0900

    added dataset provenance

--- biopax2bio2rdf.php
+++ biopax2bio2rdf.php
@@ -36,6 +36,7 @@ Class BioPAX2Bio2RDF extends RDFFactory
 	private $biopax = null;
 	private $base_ns = null;
 	private $bio2rdf_ns = null;
+	private $dataset_uri = null;
 	private $declared = null;
 	
 	function __construct()
@@ -97,6 +98,11 @@ Class BioPAX2Bio2RDF extends RDFFactory
 	function SetBio2RDFNamespace($bio2rdf_ns)
 	{
 		$this->bio2rdf_ns = $bio2rdf_ns;
+		return $this;
+	}
+	function SetDatasetURI($dataset_uri)
+	{
+		$this->dataset_uri = $dataset_uri;
 		return $this;
 	}
 	
@@ -231,6 +237,9 @@ Class BioPAX2Bio2RDF extends RDFFactory
 			} else {
 				$s_uri = str_replace($this->base_ns,$this->bio2rdf_ns,$s);
 				if(!$s_uri) continue;
+			}
+			if(isset($this->dataset_uri)) {
+				$rdf .= $this->Quad($s_uri,$nso->GetFQURI("void:inDataset"),$nso->GetFQURI($this->dataset_uri));
 			}
 			
 			if($s[0] != '_' && $s != $s_uri) $rdf .= $this->Quad($s_uri,$nso->GetFQURI("owl:sameAs"),$s);
