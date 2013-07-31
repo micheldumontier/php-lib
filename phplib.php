d95aa67faa65766ead215b3dc876035dc79f39f4
Michel Dumontier <michel.dumontier@gmail.com>
2013-07-30 21:45:58 -0400

    initial import

---
+++ phplib.php
@@ -0,0 +1,36 @@
+<?php
+/**
+Copyright (C) 2013 Michel Dumontier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+require_once(__DIR__.'/application.php');
+require_once(__DIR__.'/bio2rdfapi.php');
+require_once(__DIR__.'/biopax2bio2rdf.php');
+require_once(__DIR__.'/dataset.php');
+require_once(__DIR__.'/dataresource.php');
+require_once(__DIR__.'/fileapi.php');
+require_once(__DIR__.'/owlapi.php');
+require_once(__DIR__.'/rdfapi.php');
+require_once(__DIR__.'/registry.php');
+require_once(__DIR__.'/utils.php');
+require_once(__DIR__.'/xmlapi.php');
+
+?>
